a9cde1c406a889ab96400555a27d787bfa0df56f
tornadomeet <wuweigreat@qq.com>
2016-08-22 16:41:34 +0800

    restore train_resnet.py at begining

--- train_resnet.py
+++ train_resnet.py
@@ -45,8 +45,8 @@ def main():
     if args.retrain:
         _, arg_params, aux_params = mx.model.load_checkpoint("model/resnet-{}-{}".format(args.data_type, args.depth), args.model_load_epoch)
     train = mx.io.ImageRecordIter(
-        path_imgrec         = os.path.join(args.data_dir, "train_480_q90.rec"),
-        # path_imgrec         = os.path.join(args.data_dir, "train_256_q90.rec"),
+        # path_imgrec         = os.path.join(args.data_dir, "train_480_q90.rec"),
+        path_imgrec         = os.path.join(args.data_dir, "train_256_q90.rec"),
         label_width         = 1,
         data_name           = 'data',
         label_name          = 'softmax_label',
@@ -55,12 +55,12 @@ def main():
         pad                 = 4 if args.data_type == "cifar10" else 0,
         fill_value          = 127,  # only used when pad is valid
         rand_crop           = True,
-        # max_random_scale    = 1.0 if args.data_type == "cifar10" else 1.0,  # 480
-        # min_random_scale    = 1.0 if args.data_type == "cifar10" else 0.533,  # 256.0/480.0
-        # max_aspect_ratio    = 0 if args.data_type == "cifar10" else 0.25,
-        # random_h            = 0 if args.data_type == "cifar10" else 36,  # 0.4*90
-        # random_s            = 0 if args.data_type == "cifar10" else 50,  # 0.4*127
-        # random_l            = 0 if args.data_type == "cifar10" else 50,  # 0.4*127
+        max_random_scale    = 1.0 if args.data_type == "cifar10" else 1.0,  # 480
+        min_random_scale    = 1.0 if args.data_type == "cifar10" else 0.533,  # 256.0/480.0
+        max_aspect_ratio    = 0 if args.data_type == "cifar10" else 0.25,
+        random_h            = 0 if args.data_type == "cifar10" else 36,  # 0.4*90
+        random_s            = 0 if args.data_type == "cifar10" else 50,  # 0.4*127
+        random_l            = 0 if args.data_type == "cifar10" else 50,  # 0.4*127
         rand_mirror         = True,
         shuffle             = True,
         num_parts           = kv.num_workers,
