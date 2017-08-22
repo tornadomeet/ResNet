161a4b46e271b2ce0a5a0056731063df8b8bf968
wuwei <wu.wei@intellif.com>
2017-08-23 00:16:44 +0800

    add class_num=10 when training cifar10

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@ export MXNET_CUDNN_AUTOTUNE_DEFAULT=1
 
 # you'd better change setting with your own --data-dir, --depth, --batch-size, --gpus.
 # train cifar10
-#python -u train_resnet.py --data-dir data/cifar10 --data-type cifar10 --depth 164 --batch-size 128 --num-examples 50000 --gpus=0
-#python -u train_resnet.py --data-dir data/cifar10 --data-type cifar10 --depth 164 --batch-size 128 --num-examples 50000 --gpus=2,3,4,5,6,7
+# python -u train_resnet.py --data-dir data/cifar10 --data-type cifar10 --depth 164 \
+#       --batch-size 128 --num-classes 10 --num-examples 50000 --gpus=0,1
+#python -u train_resnet.py --data-dir data/cifar10 --data-type cifar10 --depth 164 \
+#      --batch-size 128 --num-classes 10 --num-examples 50000 --gpus=0,1,2,3,4,5,6,7
 
-## train resnet-50 
-python -u train_resnet.py --data-dir data/imagenet --data-type imagenet --depth 50 --batch-size 256 --gpus=0,1,2,3
+## train resnet-50
+python -u train_resnet.py --data-dir data/imagenet --data-type imagenet --depth 50 \
+       --batch-size 256 --gpus=0,1,2,3
